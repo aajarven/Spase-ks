4a0074f283bd44c518f2745128422ce302f6aa58
aajarven <anni.jarvenpaa@gmail.com>
2015-04-17 16:17:37 +0300

    level ja tesit

---
+++ spaseimpakt/src/main/java/spaseimpakt/data/Level.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaseimpakt.data;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Anni Järvenpää
+ */
+public class Level {
+
+    TreeMap<Integer, ArrayList<Vihu>> viholliset;
+    // TODO levelille tausta-artti
+    // TODO viesti tms levelin alkuun ehkä?
+
+    public Level() {
+        this.viholliset = new TreeMap<Integer, ArrayList<Vihu>>();
+    }
+
+    /**
+     * Lisää tasossa annetun vihollisen ilmestymään annetulla ajanhetkellä tässä
+     * levelissä
+     *
+     * @param aika ajanhetki, jolla vihollisen halutaan ilmestyvän
+     * @param vihu levelissä esiintyvä vihollinen
+     */
+    public void lisaaVihollinen(int aika, Vihu vihu) {
+        if (viholliset.containsKey(aika)) {
+            viholliset.get(aika).add(vihu);
+        } else {
+            ArrayList<Vihu> lisattava = new ArrayList<Vihu>();
+            lisattava.add(vihu);
+            viholliset.put(aika, lisattava);
+        }
+    }
+
+    /**
+     * palauttaa seuraavana ilmestymisvuorossa olevan vihollisen ja poistaa sen
+     * tästä levelistä
+     *
+     * @return Vihollinen, joka seuraavaksi ilmestyy näytölle
+     */
+    public Vihu seuraavaVihollinen() {
+        if (viholliset.isEmpty()) {
+            return null;
+        }
+        ArrayList<Vihu> palautettavat = viholliset.get(viholliset.firstKey());
+        Vihu palautettava;
+        if (palautettavat.size() == 1) {
+            palautettava = palautettavat.get(0);
+            viholliset.remove(viholliset.firstKey());
+        } else {
+            palautettava = palautettavat.get(palautettavat.size() - 1);
+            palautettavat.remove(palautettava);
+        }
+        return palautettava;
+    }
+
+    /**
+     * Kertoo, pitäisikö seuraavan vihollisen olla jo näytöllä
+     *
+     * @param nykyhetki
+     * @return true, jos seuraavan vihollisen voi jo piirtää näytölle, false jos
+     * ei
+     */
+    public boolean onkoSeuraavanVihollisenAika(int nykyhetki) {
+        if (viholliset.isEmpty()) {
+            return false;
+        } else if (viholliset.firstKey() <= nykyhetki) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Kertoo, onko kaikki tason viholliset jo haettu
+     * @return true jos uusia vihollisia ei enää ole, false jos on
+     */
+    public boolean ovatkoVihollisetLoppu() {
+        if (viholliset.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Testausta varten tarvittava metodi, joka palauttaa TreeMapin, jossa ovat tason vihut ja niiden ilmestymishetket
+     * @return tason vihut ilmestymishetken mukaan
+     */
+    public TreeMap<Integer, ArrayList<Vihu>> getVihut(){
+        return viholliset;
+    }    
+}
---
+++ spaseimpakt/src/test/java/spaseimpakt/data/LevelTest.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaseimpakt.data;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.TreeMap;
+import javax.imageio.ImageIO;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import spaseimpakt.kayttoliittyma.GraafinenKayttoliittyma;
+import spaseimpakt.logiikka.Pelimoottori;
+
+/**
+ *
+ * @author Anni Järvenpää
+ */
+public class LevelTest {
+
+    Level lvl;
+    Vihu eka;
+    Vihu toka;
+    Vihu kolmas;
+    int ilmestymisaika1;
+    int ilmestymisaika2;
+
+    public LevelTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        eka = new Pikkuvihu(1, lueSprite(), new FunktioLiikutin(1, 1, new int[]{0, 0, 0, 0, 50}), new Pelimoottori(new GraafinenKayttoliittyma()));
+        toka = new Pikkuvihu(2, lueSprite(), new FunktioLiikutin(1, 1, new int[]{0, 0, 0, 0, 50}), new Pelimoottori(new GraafinenKayttoliittyma()));
+        kolmas = new Pikkuvihu(3, lueSprite(), new FunktioLiikutin(1, 1, new int[]{0, 0, 0, 0, 50}), new Pelimoottori(new GraafinenKayttoliittyma()));
+
+        ilmestymisaika1=1;
+        ilmestymisaika2=5;
+        
+        lvl = new Level();
+        lvl.lisaaVihollinen(ilmestymisaika1, eka);
+        lvl.lisaaVihollinen(ilmestymisaika2, toka);
+        lvl.lisaaVihollinen(ilmestymisaika2, kolmas);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testLisaaVihollinen() {
+        Vihu eka = new Pikkuvihu(1, lueSprite(), new FunktioLiikutin(1, 1, new int[]{0, 0, 0, 0, 50}), new Pelimoottori(new GraafinenKayttoliittyma()));
+        Level lisaysLevel = new Level();
+        lisaysLevel.lisaaVihollinen(1, eka);
+        TreeMap<Integer, ArrayList<Vihu>> vihuMap = lisaysLevel.getVihut();
+        assertEquals("Ensimmäisen vihollisen lisääminen ei luo uutta joukkoa kyseisellä hetkellä ilmestyviä vihollisia varten", 1, vihuMap.keySet().size());
+        assertEquals("Ensimmäisen vihollisen lisäämisen jälkeen vihollisia on väärä määrä", 1, vihuMap.get(vihuMap.firstKey()).size());
+
+        Vihu toka = new Pikkuvihu(1, lueSprite(), new FunktioLiikutin(1, 1, new int[]{0, 0, 0, 0, 50}), new Pelimoottori(new GraafinenKayttoliittyma()));
+        lisaysLevel.lisaaVihollinen(5, toka);
+        assertEquals("Toisen eri aikaan ilmestyvän vihollisen lisääminen ei luo uutta joukkoa kyseisellä hetkellä ilmestyviä vihollisia varten", 2, vihuMap.keySet().size());
+        assertEquals("Toisen vihollisen lisäämisen jälkeen vihollisia on väärä määrä kyseistä aikaa vastaavassa listassa", 1, vihuMap.get(5).size());
+        assertEquals("Toisen vihollisen lisääminen muuttaa myös ensimmäisen vihollisen sisältävän ArrayListin kokoa", 1, vihuMap.get(5).size());
+        
+        Vihu kolmas = new Pikkuvihu(1, lueSprite(), new FunktioLiikutin(1, 1, new int[]{0, 0, 0, 0, 50}), new Pelimoottori(new GraafinenKayttoliittyma()));
+        lisaysLevel.lisaaVihollinen(5, kolmas);
+        assertEquals("Samaan aikaan edellisen kanssa ilmestyvän vihollisen lisääminen luo uuden avaimen vaikka sopiva on jo olemassa", 2, vihuMap.keySet().size());
+        assertEquals("Samaan aikaan edellisen kanssa ilmestyvän vihollisen lisäämisen jälkeen vihollisia on väärä määrä kyseistä aikaa vastaavassa listassa", 2, vihuMap.get(5).size());
+    }
+
+    /**
+     * Test of seuraavaVihollinen method, of class Level.
+     */
+    @Test
+    public void testSeuraavaVihollinen() {
+        assertEquals("Ensimmäisenä ei anneta aikaisimmin ilmestyvää vihollista", eka, lvl.seuraavaVihollinen());
+        Vihu ekaSamanaikainen = lvl.seuraavaVihollinen();
+        assertTrue("Toisena ei anneta jompaakumpaa seuraavaksi ilmestyvistä vihollisista", ekaSamanaikainen.equals(toka) || ekaSamanaikainen.equals(kolmas));
+        Vihu tokaSamanaikainen = lvl.seuraavaVihollinen();
+        assertFalse("seuraavaVihollinen()-metodi palauttaa kahdesti saman vihollisen", tokaSamanaikainen.equals(ekaSamanaikainen));
+        assertTrue("Kolmantena ei anneta jompaakumpaa seuraavaksi ilmestyvistä vihollisista", tokaSamanaikainen.equals(toka) || tokaSamanaikainen.equals(kolmas));
+    }
+
+    /**
+     * Test of onkoSeuraavanVihollisenAika method, of class Level.
+     */
+    @Test
+    public void testOnkoSeuraavanVihollisenAika() {
+        assertTrue("Ohjelman mukaan ei ole seuraavan aika, vaikka pitäisi olla", lvl.onkoSeuraavanVihollisenAika(ilmestymisaika1));
+        assertFalse("Ohjelman mukaan on seuraavan aika, vaikka ei pitäisi olla", lvl.onkoSeuraavanVihollisenAika(0));
+        assertTrue("Ohjlma ei tunnista, että seuraavan vihollisen pitäisi ilmestyä, vaikka sen aika olisi jo ohitettu", lvl.onkoSeuraavanVihollisenAika(ilmestymisaika1+1));
+        while(lvl.onkoSeuraavanVihollisenAika(ilmestymisaika1)){
+            lvl.seuraavaVihollinen();
+        }
+        assertTrue("Ohjelman mukaan ei ole seuraavan aika, vaikka pitäisi olla", lvl.onkoSeuraavanVihollisenAika(ilmestymisaika2));
+        assertFalse("Ohjelman mukaan on seuraavan aika, vaikka ei pitäisi olla", lvl.onkoSeuraavanVihollisenAika(ilmestymisaika2-1));
+    }
+
+    private Image lueSprite() {
+        try {
+            BufferedImage i = ImageIO.read(new File("resources/normivihu.png"));
+            return i;
+        } catch (IOException e) {
+            System.out.println("Vihollisen kuvaa ei löytynyt");
+            return null;
+        }
+    }
+}
