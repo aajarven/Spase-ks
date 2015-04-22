9b2553f68167a0e4e24972094174b1cbf408d7b9
aajarven <anni.jarvenpaa@gmail.com>
2015-04-22 12:38:56 +0300

    polut levelihtiedostoihin konstruktorille eikä kovakoodattuna

--- spaseimpakt/src/main/java/spaseimpakt/logiikka/LevelManager.java
+++ spaseimpakt/src/main/java/spaseimpakt/logiikka/LevelManager.java
@@ -12,40 +12,46 @@ import spaseimpakt.utils.JsonLukija;
 
 /**
  * Hoitaa levelin vaihtamiseen liittyvän toiminnallisuuden
+ *
  * @author Anni Järvenpää
  */
 public class LevelManager {
-    
-    private static String[] levelPolut = new String[]{"resources/levelit/ekalevel.json"};
+
+    private static String[] levelPolut;
     private JsonLukija lukija = new JsonLukija();
     private Pelimoottori moottori;
     private int nykyinenLvl;
 
     /**
      * Konstruktori,
-     * @param moottori pelimoottori, jonka pyörittämään peliin luettu level tulee
+     *
+     * @param moottori pelimoottori, jonka pyörittämään peliin luettu level
+     * tulee
      */
-    public LevelManager(Pelimoottori moottori) {
-        this.moottori=moottori;
-        nykyinenLvl=0;
+    public LevelManager(String[] levelPolut, Pelimoottori moottori) {
+        this.levelPolut = levelPolut;
+        this.moottori = moottori;
+        nykyinenLvl = 0;
     }
-    
+
     /**
-     * Mikäli levelejä on vielä jäljellä, palautetaan seuraava level. Jos ei ole, palautetaan null.
+     * Mikäli levelejä on vielä jäljellä, palautetaan seuraava level. Jos ei
+     * ole, palautetaan null.
+     *
      * @return seuraava level tai null jos viimeinen level on jo haettu.
      */
-    public Level lueSeuraavaLevel(){
+    public Level lueSeuraavaLevel() {
         nykyinenLvl++;
-        if(nykyinenLvl>levelPolut.length){
+        if (nykyinenLvl > levelPolut.length) {
             return null;
         } else {
-            Level palautettava=new Level();
-            HashMap<Vihu, Integer> vihuajat = lukija.lueVihut(levelPolut[nykyinenLvl-1], moottori);
-            for(Vihu vihu: vihuajat.keySet()){
+            Level palautettava = new Level();
+            HashMap<Vihu, Integer> vihuajat = lukija.lueVihut(levelPolut[nykyinenLvl - 1], moottori);
+            for (Vihu vihu : vihuajat.keySet()) {
                 palautettava.lisaaVihollinen(vihuajat.get(vihu), vihu);
             }
             return palautettava;
         }
     }
-    
+
 }
